b606108dce0404186266149167f8a2f72fdb8280
uptimizt <uptimizt@gmail.com>
2022-01-22 21:30:44 +0300

    .

--- Modules/QuickClosing/SvelteApp/src/App.svelte
+++ Modules/QuickClosing/SvelteApp/src/App.svelte
@@ -3,18 +3,24 @@
 	import { isLocal, dd, remoteRequest } from './helper.js';
 
     let baseUrl =  new URL('https://hd.bizio.site/conversation/ajax');
-    let context = {
-        isLocal: true,
-        conversation_id: 8791,
-        folder_id: 37,
-    }
+    let context
     
     $: {
         dd(context)
     }
 
     onMount(() => {
-        if( ! isLocal() ){
+
+        if( isLocal() ){
+            context = {
+                isLocal: true,
+                conversation_id: 8791,
+                folder_id: 37,
+                mailbox_id: 5,
+            }
+
+            context.mailboxUrl = location.origin + '/mailbox/' + context.mailbox_id + '/' + context.folder_id
+        } else {
             setContext()
         }
     })
@@ -24,7 +30,10 @@
             isLocal: false,
             conversation_id: getGlobalAttr('conversation_id'),
             folder_id: getQueryParam('folder_id'),
+            mailbox_id: getGlobalAttr('mailbox_id'),
         }
+
+        context.mailboxUrl = location.origin + '/mailbox/' + context.mailbox_id + '/' + context.folder_id
     }
 
 
@@ -55,13 +64,15 @@
         // return;
 
         remoteRequest( url, args ).then((data) => {
-				console.log(data);
+				// console.log(context.mailboxUrl);
+				// console.log(data);
+                window.location.href = context.mailboxUrl
         })
     }
 </script>
 
 <section class="app-toolbox-wrapper">
-    <div class="conv-close-action">
+    <div class="conv-actions">
         <button type="button" on:click={handleClick}>
             <i class="glyphicon glyphicon-ok-circle"></i>
         </button>
@@ -74,10 +85,10 @@
 
     }
 
-    .conv-close-action {
-        box-shadow: 0px 0px 1rem #0000002b;
+    .conv-actions {
         border-radius: 0.3rem;
-
+        display: flex;
+        justify-content: flex-end;
     }
 
     button {
